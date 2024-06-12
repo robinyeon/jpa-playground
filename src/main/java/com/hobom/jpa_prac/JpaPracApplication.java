f420e43dd2a4f1e9c376f5dfd0f2d5c8b49a6f0e
robinyeon <robin.yeon@gmail.com>
2024-06-12 15:31:09 +0900

    jpa_prac first commit

---
+++ src/main/java/com/hobom/jpa_prac/JpaPracApplication.java
@@ -0,0 +1,13 @@
+package com.hobom.jpa_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPracApplication.class, args);
+	}
+
+}
